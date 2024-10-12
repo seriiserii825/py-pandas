7a61b78d1d8b9313e24f2270ce2d52494f277f44
serii burduja <seriiburduja@gmail.com>
2024-10-12 11:04:29 +0300

    feat: pandas finished

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pandas
-# import csv
 file_path = 'data.csv'
-#
-# data = []
-#
-# with open(file_path) as file:
-#     data = csv.reader(file)
-#     temperatures = []
-#     for index, row in enumerate(data):
-#         if index != 0:
-#             temperatures.append(int(row[1]))
-# print(temperatures)
-#
-
 data = pandas.read_csv(file_path)
-print(f"data: {data}")
-print(data['temp'])
+#print(data['temp'])
+
+data_dict = data.to_dict()
+#print(f"data_dict: {data_dict}")
+
+all_temps = data['temp'].to_list()
+average_temp = sum(all_temps) / len(all_temps)
+#print(f"average_temp: {round(average_temp, 1)}")
+max_temp = data['temp'].max()
+#print(f"max_temp: {max_temp}")
+min_temp = data['temp'].min()
+#print(f"min_temp: {min_temp}")
+
+# print(data[data.temp == max_temp])
+
+monday = data[data.day == 'Monday']
+#print(f"monday: {monday}")
+monday_temp = monday.temp.to_list()[0] * 9/5 + 32
+#print(f"monday_temp: {monday_temp}")
