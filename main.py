67978076f495630e8113f7fc8e261cd09ceabd9c
serii burduja <seriiburduja@gmail.com>
2024-10-12 10:36:44 +0300

    feat: pandas

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import csv
+import pandas
+# import csv
 file_path = 'data.csv'
+#
+# data = []
+#
+# with open(file_path) as file:
+#     data = csv.reader(file)
+#     temperatures = []
+#     for index, row in enumerate(data):
+#         if index != 0:
+#             temperatures.append(int(row[1]))
+# print(temperatures)
+#
 
-data = []
-
-with open(file_path) as file:
-    data = csv.reader(file)
-    temperatures = []
-    for index, row in enumerate(data):
-        if index != 0:
-            temperatures.append(int(row[1]))
-print(temperatures)
-
+data = pandas.read_csv(file_path)
+print(f"data: {data}")
+print(data['temp'])
