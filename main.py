0a5bc0a4e94de4d7b9cab1907f2c7178552ab51a
serii burduja <seriiburduja@gmail.com>
2024-10-14 12:45:36 +0300

    feat: exit

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
+import turtle
+import pandas
 from myVersion import myVersion
 
+# myVersion()
 
-myVersion()
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "states.gif"
+screen.setup(width=725, height=491, startx=20, starty=20)
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+    if answer_state is None:
+        break
+    if answer_state == 'exit':
+        break
+
+    answer_state = answer_state.title()
+    #print(f"answer_state: {answer_state}")
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+# turtle.mainloop()
