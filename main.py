ea1620f0ada94653ab8abf8488f44437c96ad452
serii burduja <seriiburduja@gmail.com>
2024-10-14 12:50:08 +0300

    feat: states to learn

--- main.py
+++ main.py
@@ -34,4 +34,11 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
 
+# states to learn
+states_to_learn = [state for state in all_states if state not in guessed_states]
+print(f"states_to_learn: {states_to_learn}")
+
+new_data = pandas.DataFrame(states_to_learn)
+new_data.to_csv("states_to_learn.csv")
+
 # turtle.mainloop()
